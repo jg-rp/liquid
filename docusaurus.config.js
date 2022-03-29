b5b13553f0318908d64f9f293f8507b6ea60f0e3
James Prior <jamesgr.prior@gmail.com>
2022-03-29 08:52:36 +0100

    Swizzle prism languages with custom liquid definition.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,44 +1,43 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Python Liquid',
-  tagline: 'Python Liquid Template Engine',
-  url: 'https://jg-rp.github.io/',
-  baseUrl: '/liquid/',  // /liquid/ for gh-pages?
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'jg-rp', // Usually your GitHub org/user name.
-  projectName: 'liquid', // Usually your repo name.
+  title: "Python Liquid",
+  tagline: "Python Liquid Template Engine",
+  url: "https://jg-rp.github.io/",
+  baseUrl: "/liquid/", // /liquid/ for gh-pages?
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "jg-rp", // Usually your GitHub org/user name.
+  projectName: "liquid", // Usually your repo name.
   trailingSlash: false,
 
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/jg-rp/liquid/tree/docs',
-          routeBasePath: '/',
+          editUrl: "https://github.com/jg-rp/liquid/tree/docs",
+          routeBasePath: "/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/jg-rp/liquid/tree/gh-pages/blog/',
+          editUrl: "https://github.com/jg-rp/liquid/tree/gh-pages/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         sitemap: {
-          changefreq: 'weekly',
+          changefreq: "weekly",
           priority: 0.5,
         },
       }),
@@ -49,58 +48,58 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Python Liquid',
+        title: "Python Liquid",
         logo: {
-          alt: 'Python Liquid Logo',
-          src: 'img/droplet_blue_with_yellow_liquid.svg',
-          srcDark: 'img/droplet_yellow_with_blue_liquid.svg',
+          alt: "Python Liquid Logo",
+          src: "img/droplet_blue_with_yellow_liquid.svg",
+          srcDark: "img/droplet_yellow_with_blue_liquid.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'introduction/getting-started',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "introduction/getting-started",
+            position: "left",
+            label: "Docs",
           },
           {
-            type: 'doc',
-            docId: 'language/filters',
-            position: 'left',
-            label: 'Filters',
+            type: "doc",
+            docId: "language/filters",
+            position: "left",
+            label: "Filters",
           },
           {
-            type: 'doc',
-            docId: 'language/tags',
-            position: 'left',
-            label: 'Tags',
+            type: "doc",
+            docId: "language/tags",
+            position: "left",
+            label: "Tags",
           },
           {
-            type: 'doc',
-            docId: 'api/Environment',
-            position: 'left',
-            label: 'API',
+            type: "doc",
+            docId: "api/Environment",
+            position: "left",
+            label: "API",
           },
           // {to: '/blog', label: 'Blog', position: 'right'},
           {
-            href: 'https://github.com/jg-rp/liquid/',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/jg-rp/liquid/",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} James Prior. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['liquid']
+        additionalLanguages: ["django"],
       },
       algolia: {
-        appId: 'SY0LPESLUP',
-        apiKey: '5f0c1de3dad3648085c7bcd9fd6c4997',
-        indexName: 'jg-rp',
+        appId: "SY0LPESLUP",
+        apiKey: "5f0c1de3dad3648085c7bcd9fd6c4997",
+        indexName: "jg-rp",
         contextualSearch: false,
       },
     }),
