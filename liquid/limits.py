5bcc234ba48dc24b237fdd5bfc961c31d5019752
James Prior <jamesgr.prior@gmail.com>
2022-09-10 08:51:38 +0100

    future-proof str to int limit

--- liquid/limits.py
+++ liquid/limits.py
@@ -7,17 +7,47 @@ resulting in too many breaking changes.
 """
 
 import os
+import sys
+
 from typing import Any
 
 from liquid.exceptions import LiquidValueError
 
 
-MAX_STR_INT = int(
-    os.environ.get(
-        "LIQUIDINTMAXSTRDIGITS",
-        os.environ.get("PYTHONINTMAXSTRDIGITS", "4300"),
+_MIN_STR_INT = 640
+_DEFAULT_MAX_STR_INT = 4300
+
+
+def _init_int_max_str_digits() -> int:
+    var = "LIQUIDINTMAXSTRDIGITS"
+    env = os.environ.get(var)
+
+    if env is None:
+        if hasattr(sys, "get_int_max_str_digits"):
+            return sys.get_int_max_str_digits()  # type: ignore
+
+        var = "PYTHONINTMAXSTRDIGITS"
+        env = os.environ.get(var)
+
+    if env is None:
+        return _DEFAULT_MAX_STR_INT
+
+    if not env.isdigit():
+        raise TypeError(
+            f"{var}: invalid limit; must be >= " f"{_MIN_STR_INT} or 0 for unlimited"
+        )
+
+    max_digits = int(env)
+
+    if max_digits == 0 or max_digits >= _MIN_STR_INT:
+        return max_digits
+
+    raise ValueError(
+        f"{var}: invalid limit; must be >= {_MIN_STR_INT} or 0 for unlimited"
     )
-)
+
+
+MAX_STR_INT = _init_int_max_str_digits()
 
 
 def to_int(val: Any) -> int:
@@ -25,7 +55,11 @@ def to_int(val: Any) -> int:
 
     See https://github.com/python/cpython/issues/95778
     """
-    if isinstance(val, (str, bytes, bytearray)) and len(val) > MAX_STR_INT:
+    if (
+        isinstance(val, (str, bytes, bytearray))
+        and MAX_STR_INT != 0
+        and len(val) > MAX_STR_INT
+    ):
         raise LiquidValueError(
             f"integer string conversion limit ({MAX_STR_INT}) reached: "
             f"value has {len(val)} digits"
--- tests/test_large_str_to_int.py
+++ tests/test_large_str_to_int.py
@@ -1,11 +1,16 @@
 # pylint: disable=missing-class-docstring missing-module-docstring
+import os
 import unittest
+
+from mock import patch
 
 from liquid import Template
 from liquid import Mode
 
 from liquid.exceptions import LiquidValueError
+
 from liquid.limits import MAX_STR_INT
+from liquid.limits import _init_int_max_str_digits
 
 
 class StrToIntTestCase(unittest.TestCase):
@@ -28,3 +33,36 @@ class StrToIntTestCase(unittest.TestCase):
 
         with self.assertRaises(LiquidValueError):
             self.assertEqual(template.render(), "1" * (MAX_STR_INT + 1))
+
+    @patch.dict(os.environ, {"LIQUIDINTMAXSTRDIGITS": "hello"})
+    def test_liquid_max_not_digit(self):
+        """Test that we handle liquid environment variables that not digits."""
+        with self.assertRaises(TypeError):
+            _init_int_max_str_digits()
+
+    @patch.dict(os.environ, {"PYTHONINTMAXSTRDIGITS": "hello"})
+    def test_python_max_not_digit(self):
+        """Test that we handle environment variables that not digits."""
+        with self.assertRaises(TypeError):
+            _init_int_max_str_digits()
+
+    @patch.dict(os.environ, {"LIQUIDINTMAXSTRDIGITS": "5"})
+    def test_max_digits_too_small(self):
+        """Test that we handle environment variables that are too small."""
+        with self.assertRaises(ValueError):
+            _init_int_max_str_digits()
+
+    @patch.dict(os.environ, {"LIQUIDINTMAXSTRDIGITS": "1000"})
+    def test_set_liquid_max(self):
+        """Test that we can set LIQUIDINTMAXSTRDIGITS."""
+        self.assertEqual(_init_int_max_str_digits(), 1000)
+
+    @patch.dict(os.environ, {"PYTHONINTMAXSTRDIGITS": "1000"})
+    def test_python_max(self):
+        """Test that we can set PYTHONINTMAXSTRDIGITS."""
+        self.assertEqual(_init_int_max_str_digits(), 1000)
+
+    @patch.dict(os.environ, {"LIQUIDINTMAXSTRDIGITS": "0"})
+    def test_zero_is_ok(self):
+        """Test that zero is OK"""
+        self.assertEqual(_init_int_max_str_digits(), 0)
