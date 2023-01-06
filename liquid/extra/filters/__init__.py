b201d5c5d0c62f269ee23ad7651d60e9e378428b
James Prior <jamesgr.prior@gmail.com>
2023-01-06 07:38:35 +0000

    Extra `sort_numeric` filter.

--- liquid/extra/filters/__init__.py
+++ liquid/extra/filters/__init__.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-module-docstring
 from .array import index
+from .array import sort_numeric
 from .html import script_tag
 from .html import stylesheet_tag
 from ._json import JSON
@@ -9,5 +10,6 @@ __all__ = (
     "index",
     "JSON",
     "script_tag",
+    "sort_numeric",
     "stylesheet_tag",
 )
