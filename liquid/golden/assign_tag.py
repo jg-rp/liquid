f5488cc23cbbada40d1734180cccea43a239b8be
James Prior <jamesgr.prior@gmail.com>
2023-01-19 08:31:57 +0000

    Add more `assign` tag test cases.

--- liquid/golden/assign_tag.py
+++ liquid/golden/assign_tag.py
@@ -19,4 +19,27 @@ cases = [
         template=r"{% assign foo = (1..3) %}{{ foo | join: '#' }}",
         expect="1#2#3",
     ),
+    Case(
+        description="assign to variable with a hyphen",
+        template=r"{% assign some-thing = 'foo' %}{{ some-thing }}",
+        expect="foo",
+    ),
+    Case(
+        description="assign an existing array",
+        template=r"{% assign foo = bar %}{{ foo[0] }}/{{ foo[1] }}",
+        expect="a/b",
+        globals={"bar": ["a", "b", "c"]},
+    ),
+    Case(
+        description="assign an item from an existing object with quoted notation",
+        template=r"{% assign foo = bar['baz'] %}{{ foo }}",
+        expect="hello",
+        globals={"bar": {"baz": "hello"}},
+    ),
+    Case(
+        description="assign with quoted notation and extra whitespace",
+        template=r"{% assign foo = bar[ 'baz'  ] %}{{ foo }}",
+        expect="hello",
+        globals={"bar": {"baz": "hello"}},
+    ),
 ]
