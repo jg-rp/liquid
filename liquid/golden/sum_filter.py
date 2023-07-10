256365c0170b45434bbd3d04e9d38f340e250a3d
James Prior <jamesgr.prior@gmail.com>
2023-07-10 13:04:17 +0100

    Avoid testing summation of floats.

--- liquid/golden/sum_filter.py
+++ liquid/golden/sum_filter.py
@@ -27,24 +27,24 @@ cases = [
         expect="-6",
         globals={"a": [-1, -2, -3]},
     ),
-    Case(
-        description="floats",
-        template=r"{{ a | sum }}",
-        expect="0.6",
-        globals={"a": [0.1, 0.2, 0.3]},
-    ),
-    Case(
-        description="float strings",
-        template=r"{{ a | sum }}",
-        expect="0.6",
-        globals={"a": ["0.1", "0.2", "0.3"]},
-    ),
-    Case(
-        description="floats and ints",
-        template=r"{{ a | sum }}",
-        expect="6.6",
-        globals={"a": [0.1, 0.2, 0.3, 1, 2, 3]},
-    ),
+    # Case(
+    #     description="floats",
+    #     template=r"{{ a | sum }}",
+    #     expect="0.6",
+    #     globals={"a": [0.1, 0.2, 0.3]},
+    # ),
+    # Case(
+    #     description="float strings",
+    #     template=r"{{ a | sum }}",
+    #     expect="0.6",
+    #     globals={"a": ["0.1", "0.2", "0.3"]},
+    # ),
+    # Case(
+    #     description="floats and ints",
+    #     template=r"{{ a | sum }}",
+    #     expect="6.6",
+    #     globals={"a": [0.1, 0.2, 0.3, 1, 2, 3]},
+    # ),
     Case(
         description="negative strings",
         template=r"{{ a | sum }}",
