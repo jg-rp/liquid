7b43838b2948f1531ac6f6633e280fd49785ae44
James Prior <jamesgr.prior@gmail.com>
2023-09-02 09:59:57 +0100

    Test empty `for` block

--- tests/test_suppress_whitespace_only_blocks.py
+++ tests/test_suppress_whitespace_only_blocks.py
@@ -34,6 +34,11 @@ class DefaultWhitespaceSuppressionTestCase(unittest.TestCase):
         )
         self.assertEqual(template.render(), "!!")
 
+    def test_suppress_empty_for_block(self) -> None:
+        """Test that we suppress empty for blocks by default."""
+        template = self.env.from_string("!{% for x in (1..3) %}\n \t\r{% endfor %}!")
+        self.assertEqual(template.render(), "!!")
+
 
 class MyEnvironment(Environment):
     render_whitespace_only_blocks = True
@@ -45,26 +50,31 @@ class ControlWhitespaceSuppressionTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.env = MyEnvironment()
 
-    def test_suppress_empty_if_block(self) -> None:
+    def test_output_empty_if_block(self) -> None:
         """Test that we can output empty if blocks."""
         template = self.env.from_string("!{% if true %}\n \t\r{% endif %}!")
         self.assertEqual(template.render(), "!\n \t\r!")
 
-    def test_suppress_empty_else_block(self) -> None:
+    def test_output_empty_else_block(self) -> None:
         """Test that we can output empty else blocks."""
         template = self.env.from_string(
             "!{% if false %}foo{% else %}\n \r\t{% endif %}!"
         )
         self.assertEqual(template.render(), "!\n \r\t!")
 
-    def test_suppress_empty_unless_block(self) -> None:
+    def test_output_empty_unless_block(self) -> None:
         """Test that we can output empty unless blocks."""
         template = self.env.from_string("!{% unless false %}\n \t\r{% endunless %}!")
         self.assertEqual(template.render(), "!\n \t\r!")
 
-    def test_suppress_empty_case_block(self) -> None:
+    def test_output_empty_case_block(self) -> None:
         """Test that we can output empty case blocks."""
         template = self.env.from_string(
             "!{% assign x = 1 %}{% case x %}{% when 1 %}\n \t\r{% endcase %}!"
         )
         self.assertEqual(template.render(), "!\n \t\r!")
+
+    def test_output_empty_for_block(self) -> None:
+        """Test that we can output empty for blocks."""
+        template = self.env.from_string("!{% for x in (1..3) %}\n{% endfor %}!")
+        self.assertEqual(template.render(), "!\n\n\n!")
