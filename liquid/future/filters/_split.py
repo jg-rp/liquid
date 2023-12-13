a0dc1cc0b794a5cdd526d82d908ebd413aeb9349
James Prior <jamesgr.prior@gmail.com>
2023-12-13 19:25:39 +0000

    Add more to doc comment.

--- liquid/future/filters/_split.py
+++ liquid/future/filters/_split.py
@@ -8,7 +8,12 @@ from liquid.filter import string_filter
 
 @string_filter
 def split(val: str, sep: str) -> List[str]:
-    """Split string _val_ on delimiter _sep_."""
+    """Split string _val_ on delimiter _sep_.
+
+    If _sep_ is empty or _undefined_, _val_ is split into a list of single
+    characters. If _val_ is empty or equal to _sep_, and empty list is
+    returned.
+    """
     if not sep:
         return list(val)
 
